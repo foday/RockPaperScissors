f2ddeb584f7460a4f1fccb18f45eeee5818f7eb2
foday <foday_s@yahoo.com>
2020-12-15 12:16:32 -0600

    Computer stores functions then executes
    Description: The computer goes through the whole program stores the functions see which function to execute by seeing what the input is. The is_win function is for the player, if the player wins, the play function returns the is_win if statement and returns 'you won'

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -10,3 +10,9 @@ def play():
 
     #r > s, s > p, p > r
 
+def is_win(player, opponent):
+    #return true if player wins
+    #r > s, s > p, p > r
+    if (player == 'r' and opponent == 's') or (player == 's' and opponent == 'p') \
+        or (player == 'p' and opponent == 'r'):
+        return True
