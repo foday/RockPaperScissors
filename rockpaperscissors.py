5fc0580172113e0a4b4cd974a8cd404bbb2f12fd
foday <foday_s@yahoo.com>
2020-12-15 12:04:52 -0600

    Rules set up with an if statement
    The rule setup with the 'if' statement says that if the user and the computer have the same character or choice the if statement should return a 'tie' comment. A comment to define the rule further is placed on the bottom of the if statement

--- rockpaperscissors.py
+++ rockpaperscissors.py
@@ -3,3 +3,10 @@ import random
 def play(): 
     user = input("'r' for rock, 'p' for paper, 's' for scissors")
     computer = random.choice(['r', 'p', 's'])
+
+    #Rules 
+    if user == computer: 
+        return 'tie'
+
+    #r > s, s > p, p > r
+
